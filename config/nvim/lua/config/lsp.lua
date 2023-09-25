86275c94e4f2088f06048b8a403f59cabe5cfea4
shivakishore14 <shivakishore14@gmail.com>
2023-09-25 21:00:31 +0530

    Add autocomplete functionality

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -4,6 +4,50 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
+local lspkind = require'lspkind'
+local cmp = require'cmp'
+
+cmp.setup {
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'luasnip' },
+    { name = 'buffer', keyword_length = 5 }
+  },
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  formatting = {
+    -- Set up nice formatting for your sources.
+    format = lspkind.cmp_format {
+      with_text = true,
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        luasnip = "[snip]",
+        gh_issues = "[issues]",
+        tn = "[TabNine]",
+        eruby = "[erb]",
+        cody = "[cody]",
+      },
+    },
+  },
+}
+
+-- auto completion config
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 -- lsp key bindings
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...)
--- config/nvim/lua/packages.lua
+++ config/nvim/lua/packages.lua
@@ -16,4 +16,17 @@ require('packer').startup(function()
   }
   use 'kdheepak/lazygit.nvim'
   use 'EdenEast/nightfox.nvim'
+  use 'onsails/lspkind.nvim'
+
+  -- Autocompletion
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/nvim-cmp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  -- Autocompletion end
+
 end)

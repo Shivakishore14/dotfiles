ea5b9e9f6c94c35c25ec98a03b39b705953d6c60
shivakishore14 <shivakishore14@gmail.com>
2024-04-20 01:02:58 +0530

    Update keybindings

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -1,98 +1,98 @@
 -- lsp config
 local lsp_flags = {
-  -- This is the default in Nvim 0.7+
-  debounce_text_changes = 150,
+	-- This is the default in Nvim 0.7+
+	debounce_text_changes = 150,
 }
 
-local lspkind = require'lspkind'
-local cmp = require'cmp'
-require("nvim-lsp-installer").setup {}
+local lspkind = require("lspkind")
+local cmp = require("cmp")
+require("nvim-lsp-installer").setup({})
 
-cmp.setup {
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'luasnip' },
-    { name = 'buffer', keyword_length = 5 }
-  },
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-  formatting = {
-    -- Set up nice formatting for your sources.
-    format = lspkind.cmp_format {
-      with_text = true,
-      menu = {
-        buffer = "[buf]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[api]",
-        path = "[path]",
-        luasnip = "[snip]",
-        gh_issues = "[issues]",
-        tn = "[TabNine]",
-        eruby = "[erb]",
-        cody = "[cody]",
-      },
-    },
-  },
-}
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip" },
+		{ name = "buffer", keyword_length = 5 },
+	},
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	formatting = {
+		-- Set up nice formatting for your sources.
+		format = lspkind.cmp_format({
+			with_text = true,
+			menu = {
+				buffer = "[buf]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[api]",
+				path = "[path]",
+				luasnip = "[snip]",
+				gh_issues = "[issues]",
+				tn = "[TabNine]",
+				eruby = "[erb]",
+				cody = "[cody]",
+			},
+		}),
+	},
+})
 
 -- auto completion config
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- lsp key bindings
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-  local opts = {noremap = true, silent = true}
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>law', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>lrw', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>llw', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>lrn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<leader>lrf', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<leader>ld', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '<leader>ll', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '<leader>lca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+	local opts = { noremap = true, silent = true }
+	buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+	buf_set_keymap("n", "<leader>law", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<leader>lrw", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<leader>llw", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+	buf_set_keymap("n", "<leader>lt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	buf_set_keymap("n", "<leader>lrn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<leader>lrf", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "<leader>ld", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+	buf_set_keymap("n", "<leader>ll", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+	buf_set_keymap("n", "<leader>lca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 end
 
-require'lspconfig'.pyright.setup{
-  capabilities = capabilities,
-  on_attach = on_attach,
-  flags = lsp_flags,
-}
-require'lspconfig'.tsserver.setup{
-  capabilities = capabilities,
-  on_attach = on_attach,
-  flags = lsp_flags,
-}
-require'lspconfig'.gopls.setup{
-  capabilities = capabilities,
-  on_attach = on_attach,
-  flags = lsp_flags,
-}
-require'lspconfig'.tailwindcss.setup{
-  capabilities = capabilities,
-  on_attach = on_attach,
-  flags = lsp_flags,
-}
+require("lspconfig").pyright.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	flags = lsp_flags,
+})
+require("lspconfig").tsserver.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	flags = lsp_flags,
+})
+require("lspconfig").gopls.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	flags = lsp_flags,
+})
+require("lspconfig").tailwindcss.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	flags = lsp_flags,
+})
 
 require("mason").setup()
--- config/nvim/lua/keybinding.lua
+++ config/nvim/lua/keybinding.lua
@@ -1,16 +1,30 @@
 local keymap = vim.api.nvim_set_keymap
 local keymap_opts = { noremap = true }
 
-keymap('n', '<c-s>', ':w<CR>', keymap_opts)
-keymap('i', '<c-s>', '<Esc>:w<CR>a', keymap_opts)
+-- Clear highlight on escape
+keymap("n", "<Esc>", "<cmd>nohlsearch<CR>", keymap_opts)
 
-keymap('n', '<c-j>', '<c-w>j', keymap_opts)
-keymap('n', '<c-h>', '<c-w>h', keymap_opts)
-keymap('n', '<c-k>', '<c-w>k', keymap_opts)
-keymap('n', '<c-l>', '<c-w>l', keymap_opts)
-keymap('n', '<c-n>', ':NvimTreeToggle<CR>', keymap_opts)
+-- ctrl - s => save
+keymap("n", "<c-s>", ":w<CR>", keymap_opts)
+keymap("i", "<c-s>", "<Esc>:w<CR>a", keymap_opts)
 
-keymap('n', '<leader>ff', ':Telescope find_files<cr>', keymap_opts)
-keymap('n', '<leader>fg', ':Telescope live_grep<cr>', keymap_opts)
-keymap('n', '<leader>fb', ':Telescope buffers<cr>', keymap_opts)
-keymap('n', '<leader>gg', ':LazyGit<cr>', keymap_opts)
+keymap("n", "<c-j>", "<c-w>j", keymap_opts)
+keymap("n", "<c-h>", "<c-w>h", keymap_opts)
+keymap("n", "<c-k>", "<c-w>k", keymap_opts)
+keymap("n", "<c-l>", "<c-w>l", keymap_opts)
+keymap("n", "<c-n>", ":NvimTreeToggle<CR>", keymap_opts)
+
+-- Telescope keymaps
+keymap("n", "<leader>ff", ":Telescope find_files<cr>", keymap_opts)
+keymap("n", "<leader>fg", ":Telescope live_grep<cr>", keymap_opts)
+keymap("n", "<leader>fb", ":Telescope buffers<cr>", keymap_opts)
+keymap("n", "<leader>ht", ":Telescope help_tags<cr>", keymap_opts)
+
+-- Lazy git
+keymap("n", "<leader>gg", ":LazyGit<cr>", keymap_opts)
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
